9bf262f2a97b17a8c2b73a634e2c62295139430b
Iago Engel Serafim <iagoengel@yahoo.com>
2022-07-06 08:36:00 -0300

    feat(nested navigation): start

--- flutter_nuvigator/lib/main.dart
+++ flutter_nuvigator/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:nested_nuvigators/screens/display_screen.dart';
+import 'package:nested_nuvigators/screens/input_screen.dart';
+import 'package:nested_nuvigators/screens/one_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:nested_nuvigators/screens/three_screen.dart';
+import 'package:nested_nuvigators/screens/two_screen.dart';
 import 'package:nuvigator/next.dart';
-import 'package:proj/router.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,43 +14,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'Montserrat',
+        primarySwatch: Colors.purple,
       ),
-      home: Nuvigator(router: MyRouter()),
-      // home: Nuvigator.routes(
-      //     screenType: materialScreenType,
-      //     initialRoute: 'home',
-      //     routes: [
-      //       NuRouteBuilder(path: 'home', builder: (_, __, ___) => HomeScreen()),
-      //       NuRouteBuilder(
-      //           path: 'login', builder: (_, __, ___) => LoginScreen()),
-      //       NuRouteBuilder(
-      //           path: 'sing-up', builder: (_, __, ___) => SingupScreen()),
-      //       NuRouteBuilder(
-      //           path: 'favorites', builder: (_, __, ___) => FavoritesScreen()),
-      //       NuRouteBuilder(
-      //           path: 'profile', builder: (_, __, ___) => ProfileScreen()),
-      //       NuRouteBuilder(
-      //           path: 'payment', builder: (_, __, ___) => PaymentScreen()),
-      //       NuRouteBuilder(
-      //           path: 'producer-details',
-      //           builder: (_, __, NuRouteSettings args) {
-      //             final Producer producer = args.rawParameters['producer'];
-      //             return ProducerDetailsScreen(producer: producer);
-      //           }),
-      //       NuRouteBuilder(
-      //           path: 'package-details',
-      //           builder: (_, __, NuRouteSettings args) {
-      //             final Producer producer = args.rawParameters['producer'];
-      //             final Package package = args.rawParameters['ackage'];
-      //             return PackageDetailsScreen(
-      //                 producer: producer, package: package);
-      //           }),
-      //     ]),
-      // initialRoute: 'home',
-      // onGenerateRoute: RouteGenerator.generateRoutes,
+      home: Nuvigator.routes(
+        initialRoute: 'home',
+        screenType: materialScreenType,
+        routes: [
+          NuRouteBuilder(path: 'home', builder: (_, __, ___) => HomeScreen()),
+          NuRouteBuilder(path: 'one', builder: (_, __, ___) => OneScreen()),
+          NuRouteBuilder(path: 'two', builder: (_, __, ___) => TwoScreen()),
+          NuRouteBuilder(path: 'three', builder: (_, __, ___) => ThreeScreen()),
+          NuRouteBuilder(path: 'input', builder: (_, __, ___) => InputScreen()),
+          NuRouteBuilder(path: 'display', builder: (_, __, ___) => DisplayScreen()),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final nuvigator = Nuvigator.of(context);
+
+    return Scaffold(
+       appBar: AppBar(
+         title: Text("Flutter app"),
+       ),
+       body: Center(
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             Text(
+               'Toque no botão para mudar de página',
+             ),
+             SizedBox(
+               height: 20,
+             ),
+             ElevatedButton(
+               child: Text('Primeiro fluxo'),
+               onPressed: () => nuvigator.open('one'),
+             ),
+             SizedBox(
+               height: 20,
+             ),
+             ElevatedButton(
+               child: Text('Segundo fluxo'),
+               onPressed: () => nuvigator.open('input')
+             ),
+           ],
+         ),
+       ), //
     );
   }
 }
